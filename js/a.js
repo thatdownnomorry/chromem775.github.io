e456a7def221aa94193197253579afe76cdfa03d
chromem16 <93473258+chromem16@users.noreply.github.com>
2022-07-19 21:55:30 +0900

    Add files via upload

---
+++ js/a.js
@@ -0,0 +1,6 @@
+function yes(){
+         location.href="html/ps.html";
+}
+function no(){
+         location.href="https://google.com/";
+}
---
+++ js/ps.js
@@ -0,0 +1,8 @@
+{
+     mp=prompt("パスワードを入力してください。","");
+     if ( mp == "tesuto" ){
+         location.href="a8apl-joijao_an8yh8h2377139gdoi.html";
+     }else{
+         location.href="../index.html";
+     }
+}

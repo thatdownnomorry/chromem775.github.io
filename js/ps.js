543b2aba264e368844f3f751a3e76105dddbad12
chromem775 <93473258+chromem775@users.noreply.github.com>
2022-09-19 18:22:04 +0900

    Update ps.js

--- js/ps.js
+++ js/ps.js
@@ -1,6 +1,6 @@
 {
      mp=prompt("パスワードを入力してください。","");
-     if ( mp == "tesuto" ){
+     if ( mp == "umauma" ){
          location.href="a8apl-joijao_an8yh8h2377139gdoi.html";
      }else{
          location.href="../index.html";
